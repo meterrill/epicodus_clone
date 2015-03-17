2a20aeb815cdcaebb18fdc9323d9f8d80e5c833e
Matthew Terrill <meterrill@gmail.com>
2015-03-17 15:07:58 -0700

    add capybara and did integration testing

---
+++ spec/features/add_lesson_pages_spec.rb
@@ -0,0 +1,24 @@
+require 'rails_helper'
+
+describe "the add lesson process" do
+  it "adds a new lesson" do
+    visit lessons_path
+    click_on 'Add lesson'
+    fill_in 'Name', :with => 'New Lesson'
+    fill_in 'Text', :with => 'Lorem ipsum'
+    click_on 'Create Lesson'
+    expect(page).to have_content 'Lessons'
+  end
+
+  it "gives error when no name is entered" do
+    visit new_lesson_path
+    click_on 'Create Lesson'
+    expect(page).to have_content 'errors'
+  end
+
+  it "returns to lesson list, without adding a new lesson" do
+    visit new_lesson_path
+    click_on 'List lessons'
+    expect(page).to have_content 'Lessons'
+  end
+end
