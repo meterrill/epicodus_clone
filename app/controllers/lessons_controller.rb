5e3c202741626cd0f08e56ee45924edae35af019
Matthew Terrill <meterrill@gmail.com>
2015-03-17 15:09:59 -0700

    add form to allow users adding lessons

--- app/controllers/lessons_controller.rb
+++ app/controllers/lessons_controller.rb
@@ -6,4 +6,22 @@ class LessonsController < ApplicationController
   def show
     @lesson = Lesson.find(params[:id])
   end
+
+  def new
+    @lesson = Lesson.new
+  end
+
+  def create
+    @lesson = Lesson.new(lesson_params)
+    if @lesson.save
+      redirect_to lessons_path
+    else
+      render :new
+    end
+  end
+
+  private
+  def lesson_params
+    params.require(:lesson).permit(:name, :text)
+  end
 end
